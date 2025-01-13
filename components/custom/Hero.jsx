0d2e513e458a055e10ef79fbd1ea8d6f03e32872
Adithyenkandasamy <adithyen1@gmail.com>
2025-01-13 18:24:03 +0530

    want to format

--- components/custom/Hero.jsx
+++ components/custom/Hero.jsx
@@ -1,10 +1,18 @@
 import Lookup from '@/data/Lookup'
+import { ArrowRight } from 'lucide-react'
 import React from 'react'
 
 function Hero() {
   return (
-    <div>
-      <h2>{Lookup.HERO_HEADING}</h2>
+    <div className='flex flex-col items-center mt-36 xl:mt-56 gap-2'>
+      <h2 className='font-bold text-4xl'>{Lookup.HERO_HEADING}</h2>
+      <p className='text-gray-400 font-medium'>{Lookup.HERO_DESC}</p>
+      <div>
+      <div className='flex gap-2'> 
+        <textarea placeholder={Lookup.INPUT_PLACEHOLDER}/>
+        <ArrowRight className='bg-blue-500 p-2 h-8 w-8 round-md cursor-pointer'/>
+      </div>
+      </div>
     </div>
   )
 }

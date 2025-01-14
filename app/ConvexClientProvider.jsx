e8a3e203baa92f3c77addb1db315be6a00ff1ced
Adithyenkandasamy <adithyen1@gmail.com>
2025-01-14 18:17:05 +0530

    solved

--- app/ConvexClientProvider.jsx
+++ app/ConvexClientProvider.jsx
@@ -1,14 +1,16 @@
-"use client"
+"use client";
 import React from 'react';
-import { ConvexProvider,ConvexReactClient } from "convex/react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-function ConvexClientProvider({children}) {
-    const convex = ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
-  return (
-    <ConvexProvider client={convex}>
-        {children}
-    </ConvexProvider>    
-  )
+function ConvexClientProvider({ children }) {
+    // Instantiate the ConvexReactClient with 'new'
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL); // Use 'new' keyword
+    
+    return (
+        <ConvexProvider client={convex}>
+            {children}
+        </ConvexProvider>    
+    );
 }
 
-export default ConvexClientProvider
+export default ConvexClientProvider;

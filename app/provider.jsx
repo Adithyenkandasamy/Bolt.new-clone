c9d65d997a3fd3d26df1c71e74301cd7e24e7f81
Adithyenkandasamy <adithyen1@gmail.com>
2025-01-14 14:13:24 +0530

    client id error

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,10 +1,10 @@
 "use client";
-import React, { useState } from 'react'; // Make sure to import useState
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import Header from '@/components/custom/Header';
-import { MessagesContext } from '@/context/MessagesContext';
-import { UserDetailContext } from '@/context/UserDetailContext';
-import { GoogleOAuthProvider } from 'react-use-googlelogin';
+import React, { useState } from 'react' // Make sure to import useState
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import Header from '@/components/custom/Header'
+import { MessagesContext } from '@/context/MessagesContext'
+import { UserDetailContext } from '@/context/UserDetailContext'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 function Provider({ children }) {
   const [messages, setMessages] = useState();
@@ -13,19 +13,19 @@ function Provider({ children }) {
   return (
     <div>
       <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID_KEY}>
-        <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
-          <MessagesContext.Provider value={{ messages, setMessages }}>
-            <NextThemesProvider      
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <Header />
-              {children}
-            </NextThemesProvider>
-          </MessagesContext.Provider>
-        </UserDetailContext.Provider>
+      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+        <MessagesContext.Provider value={{ messages, setMessages }}>
+          <NextThemesProvider      
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+          </NextThemesProvider>
+        </MessagesContext.Provider>
+      </UserDetailContext.Provider>
       </GoogleOAuthProvider>
     </div>
   );
--- components/custom/SignInDialog.jsx
+++ components/custom/SignInDialog.jsx
@@ -1,52 +1,44 @@
-import React from 'react'
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import Lookup from '@/data/Lookup'  
-import { Button } from '../ui/button'
+import React from 'react';
 import { useGoogleLogin } from 'react-use-googlelogin';
 import axios from 'axios';
+import { UserDetailContext } from '@/context/UserDetailContext'; // Ensure this import is correct
+import { Dialog, DialogTitle, DialogDescription } from '@/components/ui/dialog';
+import Lookup from '@/data/Lookup';  
+import { Button } from '../ui/button';
 
-function SignInDialog({openDialog,closeDialog}) {
-const {userDetail, setUserDetail} = React.useContext(UserDetailContext); 
+function SignInDialog({ openDialog, closeDialog }) {
+  const { userDetail, setUserDetail } = React.useContext(UserDetailContext); 
 
-const googleLogin = useGoogleLogin({
-  onSuccess: async (tokenResponse) => {
-    console.log(tokenResponse);
-    const userInfo = await axios.get(
-      'https://www.googleapis.com/oauth2/v3/userinfo',
-      { headers: { Authorization: 'Bearer '+tokenResponse?.access_token } },
-    );
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+      const userInfo = await axios.get(
+        'https://www.googleapis.com/oauth2/v3/userinfo',
+        { headers: { Authorization: 'Bearer ' + tokenResponse?.access_token } },
+      );
 
-    console.log(userInfo);
-    setUserDetail(userInfo?.data);
-    closeDialog(false);
-  },
-  onError: errorResponse => console.log(errorResponse),
-});
+      console.log(userInfo);
+      setUserDetail(userInfo?.data);
+      closeDialog(false);
+    },
+    onError: errorResponse => console.log(errorResponse),
+  });
+
   return (
-    <Dialog open={openDialog} onOpenChange={closeDialog }>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle></DialogTitle>
+    <Dialog open={openDialog} onOpenChange={closeDialog}>
+      <DialogTitle>Sign In</DialogTitle>
       <DialogDescription>
-        <div className="flex flex-col item-center justify-center gap-2 ">
+        <div className="flex flex-col items-center justify-center gap-2 ">
           <h2 className='font-bold text-2xl text-center text-white'>{Lookup.SIGNIN_HEADING}</h2>
           <p className='mt-2 text-center'>{Lookup.SIGNIN_SUBHEADING}</p>
-          <Button className='bg-blue-500 text-white w-44 hover:bg-blue-400 mt-2 mx-auto ' onClick={googleLogin}>Sign In With Google</Button>
+          <Button className='bg-blue-500 text-white w-44 hover:bg-blue-400 mt-2 mx-auto' onClick={googleLogin}>
+            Sign In With Google
+          </Button>
           <p>{Lookup?.SIGNIn_AGREEMENT_TEXT}</p>
         </div>
-        </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
-
-  )
+      </DialogDescription>
+    </Dialog>
+  );
 }
 
-export default SignInDialog
+export default SignInDialog;

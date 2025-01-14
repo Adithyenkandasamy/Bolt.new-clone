9170d4629b2995bbda20e2a1cc1e708ef7c3c00f
Adithyenkandasamy <adithyen1@gmail.com>
2025-01-14 13:47:59 +0530

    error solved

--- app/provider.jsx
+++ app/provider.jsx
@@ -13,19 +13,19 @@ function Provider({ children }) {
   return (
     <div>
       <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID_KEY}>
-      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
-        <MessagesContext.Provider value={{ messages, setMessages }}>
-          <NextThemesProvider      
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
-            {children}
-          </NextThemesProvider>
-        </MessagesContext.Provider>
-      </UserDetailContext.Provider>
+        <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+          <MessagesContext.Provider value={{ messages, setMessages }}>
+            <NextThemesProvider      
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Header />
+              {children}
+            </NextThemesProvider>
+          </MessagesContext.Provider>
+        </UserDetailContext.Provider>
       </GoogleOAuthProvider>
     </div>
   );

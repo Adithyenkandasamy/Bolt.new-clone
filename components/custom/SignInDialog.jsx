53787934ed5bdaa026a591efbdfde8725fabc78e
Adithyenkandasamy <adithyen1@gmail.com>
2025-01-16 19:37:10 +0530

    fixed the signin dialog

--- components/custom/SignInDialog.jsx
+++ components/custom/SignInDialog.jsx
@@ -7,6 +7,7 @@ import uuid4 from 'uuid4';
 import { Dialog, DialogContent, DialogTitle, DialogDescription } from "@/components/ui/dialog"; // Import your dialog components
 import Lookup from '@/data/Lookup'; // Import your lookup data
 import { api } from '@/convex/_generated/api';
+import { Button } from '../ui/button';
 
 function SignInDialog({ openDialog, closeDialog }) {
     const { userDetail, setUserDetail } = useContext(UserDetailContext); 
@@ -47,10 +48,14 @@ function SignInDialog({ openDialog, closeDialog }) {
             <DialogContent>
                 <DialogTitle>Sign In</DialogTitle>
                 <DialogDescription>
-                    <div className="flex flex-col items-center justify-center gap-2">
-                        <h2 className='font-bold text-2xl text-center text-white'>{Lookup.SIGNIN_HEADING}</h2>
-                        <button onClick={signIn}>Sign in with Google</button>
-                    </div>
+                <div className="flex flex-col items-center justify-center gap-2">
+                            <h2 className='font-bold text-2xl text-center text-white'>{Lookup.SIGNIN_HEADING}</h2>
+                            <p className='mt-2 text-center'>{Lookup.SIGNIN_SUBHEADING}</p>
+                            <Button className='bg-blue-500 text-white w-44 hover:bg-blue-400 mt-2 mx-auto' onClick={signIn}>
+                                Sign In With Google
+                            </Button>
+                            <p>{Lookup?.SIGNIn_AGREEMENT_TEXT}</p>
+                        </div>
                 </DialogDescription>
             </DialogContent>
         </Dialog>
